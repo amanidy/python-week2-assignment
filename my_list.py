b486d39d5d2eea9421c8a72cd66ac80316a9a5e4
Amani <amaniarnold08@gmail.com>
2024-09-04 17:04:08 +0300

    Answered the questions

---
+++ my_list.py
@@ -0,0 +1,24 @@
+my_list =[]
+
+# Appending th following values into my_list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# inserting 15in the second position of my_list
+my_list.insert(1,15)
+
+#extending my_list with the second_list
+second_list =[50,60,70]
+my_list.extend(second_list)
+
+
+#removing the last element from my_list
+my_list.remove(70)
+
+#sorting my_list in ascending order
+my_list.sort()
+
+print(my_list[3])
+
